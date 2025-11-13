5d1135f74e3e94b359ad499fa9a6b2e6f5beb5c1
Jiří Buchta <jiribuchta02@gmail.com>
2025-11-13 10:13:59 +0000

    feat: added multiple similarity metrics

--- examples/slide_embeddings.py
+++ examples/slide_embeddings.py
@@ -185,8 +185,7 @@ def process_slide(slide_path, save_path, DEVICE, MODEL_DTYPE, NUM_WORKERS, BATCH
     else: 
         print("\nSlide embeddings already exists. Skipping")
 
-def get_simmilarity(slide_path):
-
+def load_embeddings(slide_path):
     dirs_to_process = sorted([
         d for d in os.listdir(slide_path) 
         if os.path.isdir(os.path.join(slide_path, d)) and not d.startswith(".")
@@ -201,32 +200,49 @@ def get_simmilarity(slide_path):
         labels.append(dir)
     
     emb_matrix = torch.stack(embeddings, dim=0)
-    X_norm = F.normalize(emb_matrix, p=2, dim=1)
-    cosine_sim = X_norm @ X_norm.T
 
+    return (emb_matrix, labels)
 
-    df = pd.DataFrame(cosine_sim, index=labels, columns=labels)
-    df.to_csv(os.path.join(slide_path, "similarity_matrix.csv"), float_format="%.12f", index=True)
+def save_simmilarity(sim_matrix, labels, name):
+
+    df = pd.DataFrame(sim_matrix, index=labels, columns=labels)
+    df.to_csv(os.path.join(slide_path, f"{name}_similarity.csv"), float_format="%.12f", index=True)
 
     plt.figure(figsize=(10, 8))
     sns.heatmap(
         cosine_sim,
         xticklabels=labels,
         yticklabels=labels,
-        cmap="viridis",               # nebo "coolwarm", "RdYlBu", …
-        annot=True,                   # zapíše hodnoty do buněk (volitelné)
-        fmt=".2f",                    # formát čísla
+        cmap="viridis",
+        annot=True,
+        fmt=".2f",                    
         linewidths=0.5,
         linecolor="gray",
-        cbar_kws={"label": "Cosine similarity"},
+        cbar_kws={"label": f"{name} similarity"},
         annot_kws={"fontsize": 2}
     )
-    plt.title("Pairwise cosine similarity of slide embeddings")
-    plt.xticks(rotation=45, ha="right", fontsize=2)   # čitelnost popisků
-    plt.yticks(rotation=0, fontsize=2)                # y‑osa vodorovně
+    plt.title(f"Pairwise {name} similarity of slide embeddings")
+    plt.xticks(rotation=45, ha="right", fontsize=2)
+    plt.yticks(rotation=0, fontsize=2)
     plt.tight_layout()
-    plt.savefig(os.path.join(slide_path, "slide_similarity_heatmap.png"), dpi=300)
-    plt.show()
+    plt.savefig(os.path.join(slide_path, f"{name}_similarity_heatmap.png"), dpi=300)
+
+def cos_simmilarity(slide_path):
+    emb_matrix, labels = load_embeddings(slide_path)
+    X_norm = F.normalize(emb_matrix, p=2, dim=1)
+    cosine_sim = X_norm @ X_norm.T
+    save_simmilarity(cosine_sim, labels, "cos")
+
+def l1_simmilarity(slide_path):
+    emb_matrix, labels = load_embeddings(slide_path)
+    l1_distance_matrix = torch.cdist(emb_matrix1, emb_matrix2, p=1).numpy()
+    save_simmilarity(cosine_sim, labels, "L1")
+
+def l2_simmilarity(slide_path):
+    emb_matrix, labels = load_embeddings(slide_path)
+    l1_distance_matrix = torch.cdist(emb_matrix1, emb_matrix2, p=2).numpy()
+    save_simmilarity(cosine_sim, labels, "L2")
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(
@@ -322,7 +338,10 @@ def main() -> None:
     print(f"vram per worker: {VRAM_PER_WORKER:.2f}")
     print(f"=============================================")
 
-    get_simmilarity(save_path)
+    cos_simmilarity(save_path)
+    l1_simmilarity(save_path)
+    l2_simmilarity(save_path)
+
 
 if __name__ == "__main__":
     main()
