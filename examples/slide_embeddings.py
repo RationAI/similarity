c789e5bea9183363b6c7eabceeeeb4c0ebb30614
Jiří Buchta <jiribuchta02@gmail.com>
2026-02-09 09:20:19 +0100

    feat: changes for musica cluster

--- examples/slide_embeddings.py
+++ examples/slide_embeddings.py
@@ -8,6 +8,10 @@ import pandas as pd
 import numpy as np
 import requests
 import time
+import logging
+import ray.data
+import shutil
+import psutil
 
 from PIL import Image
 from typing import Any
@@ -22,6 +26,20 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import seaborn as sns
 import albumentations as A
+
+def print_memory_stats():
+    # Paměť aktuálního procesu
+    process = psutil.Process(os.getpid())
+    mem_rss = process.memory_info().rss / (1024 ** 3)  # v GB
+    
+    # Celková paměť na uzlu (dostupná pro tvůj job)
+    node_mem = psutil.virtual_memory()
+    
+    print("-" * 30)
+    print(f"RAM Usage (Process): {mem_rss:.2f} GB")
+    print(f"RAM Usage (Node Total): {node_mem.percent}% used")
+    print(f"RAM Available (Node): {node_mem.available / (1024**3):.2f} GB")
+    print("-" * 30, flush=True)
 
 def tiling(row: dict[str, Any]) -> list[dict[str, Any]]:
     return [
@@ -144,18 +162,22 @@ class TileEncoderActor:
         })
         return output_df
 
-def create_tile_embeddings(slide_path, DEVICE, MODEL_DTYPE, tile_size, BATCH_SIZE, NUM_WORKERS):
+def create_tile_embeddings(slide_path, DEVICE, MODEL_DTYPE, tile_size, BATCH_SIZE, NUM_WORKERS, save_path):
     slide_metadata, tiles_metadata = load_metadata(slide_path)
+    ray.data.DataContext.get_current().execution_options.verbose_progress = False
 
-    # needed for right integration of workers
-    PRELOAD_LIB_PATH = "/home/linuxbrew/.linuxbrew/lib/libjpeg.so.8" 
-    ld_library_path = "/home/linuxbrew/.linuxbrew/lib"
+    conda_lib = f"{os.environ.get('CONDA_PREFIX')}/lib/libjpeg.so.8"
+    brew_lib = "/home/linuxbrew/.linuxbrew/lib/libjpeg.so.8"
+    
+    # Use whichever one actually exists
+    PRELOAD_LIB_PATH = conda_lib if os.path.exists(conda_lib) else brew_lib
 
     runtime_env = {
         "env_vars": {
-            "LD_LIBRARY_PATH": f"{ld_library_path}:{os.environ.get('LD_LIBRARY_PATH', '')}",
+            "LD_LIBRARY_PATH": f"{os.path.dirname(PRELOAD_LIB_PATH)}:{os.environ.get('LD_LIBRARY_PATH', '')}",
             "LD_PRELOAD": PRELOAD_LIB_PATH
-        }
+        },
+        "excludes": ["*"] # Keep your excludes from before!
     }
 
     img = openslide.OpenSlide(slide_path).get_thumbnail((1000, 1000))
@@ -174,34 +196,53 @@ def create_tile_embeddings(slide_path, DEVICE, MODEL_DTYPE, tile_size, BATCH_SIZ
         compute=ray.data.ActorPoolStrategy(size=NUM_WORKERS),
         runtime_env=runtime_env,
     )
-    return result_ds.to_pandas()
+    
+    result_ds.repartition(1).write_parquet(save_path)
+    part_file = os.path.join(save_path, os.listdir(save_path)[0]) # vezme první soubor ve složce
+    shutil.move(part_file, save_path + "/tiles.parquet")
+    return True
+
 
 def save_tile_embeddings(save_path, tiles_df):
     if not os.path.exists(save_path):
-        os.mkdir(save_path)
+        os.makedirs(save_path)
     tiles_df.to_parquet(save_path + "/tiles.parquet", index=False)
 
 def save_slide_embeddings(save_path, slide_df):
     if not os.path.exists(save_path):
-        os.mkdir(save_path)
+        os.makedirs(save_path)
     slide_df.to_parquet(save_path + "/slide.parquet", index=False)
 
 def process_slide(slide_path, save_path, DEVICE, MODEL_DTYPE, NUM_WORKERS, BATCH_SIZE, OVERRIDE):
-    slide_metadata, tile_metadata = load_metadata(slide_path)
-    slide_name = slide_metadata.take(1)[0]["path"].split('/')[-1].split('.')[0]
-    if(OVERRIDE or not os.path.exists(save_path + '/' + slide_name)):
-        tile_embeddings = create_tile_embeddings(slide_path, DEVICE, MODEL_DTYPE, 256, BATCH_SIZE, NUM_WORKERS)
-        save_tile_embeddings(save_path + '/' + slide_name, tile_embeddings)
-    else:
-        print("\nTile embeddings already exists. Skipping")
-        tile_embeddings = load_parquet(save_path + '/' + slide_name)
+    slide_name = "Unknown"
+    try:
+        slide_metadata, tile_metadata = load_metadata(slide_path)
+        slide_name = slide_metadata.take(1)[0]["path"].split('/')[-1].split('.')[0]
 
-    if (OVERRIDE or not os.path.exists(save_path + '/' + slide_name +"/slide.parquet")):
-        slide_embeddings = create_slide_embeddings_service(slide_metadata, tile_embeddings, MODEL_DTYPE, DEVICE)
-        save_slide_embeddings(save_path + '/' + slide_name, slide_embeddings)    
-    else: 
-        print("\nSlide embeddings already exists. Skipping")
+        if(OVERRIDE or not os.path.exists(save_path + '/' + slide_name)):
+            tile_embeddings = create_tile_embeddings(slide_path, DEVICE, MODEL_DTYPE, 256, BATCH_SIZE, NUM_WORKERS, save_path + '/' + slide_name )
+        else:
+            print("\nTile embeddings already exists. Skipping", flush=True)
+            #tile_embeddings = load_parquet(save_path + '/' + slide_name)
 
+        if (OVERRIDE or not os.path.exists(save_path + '/' + slide_name +"/slide.parquet")):
+            pass
+            #slide_embeddings = create_slide_embeddings_service(slide_metadata, tile_embeddings, MODEL_DTYPE, DEVICE)
+            #save_slide_embeddings(save_path + '/' + slide_name, slide_embeddings)    
+        else: 
+            print("\nSlide embeddings already exists. Skipping")
+
+    except Exception as e:
+        # Tady je to klíčové: zapíšeme chybu, ale neukončíme skript
+        print(f"\n" + "!"*50)
+        print(f"CHYBA: Slide {slide_name} nebylo možné zpracovat.")
+        print(f"Cesta: {slide_path}")
+        print(f"Chyba: {str(e)}")
+        print("!"*50 + "\n", flush=True)
+        current_wsi_save_path = os.path.join(save_path, slide_name)
+        if slide_name != "Unknown" and os.path.exists(current_wsi_save_path):
+            import shutil
+            shutil.rmtree(current_wsi_save_path)
 
 def main() -> None:
     parser = argparse.ArgumentParser(
@@ -244,6 +285,7 @@ def main() -> None:
     )
     args = parser.parse_args()
 
+    print_memory_stats()
 
     #slide_path = '/mnt/data/scans/AI scans/Comparison_of_scanners/breast/FLASH2021_6802-01-T.mrxs'
     #python -m examples.slide_embeddings --slide-path "/mnt/data/MOU/breast/comparison_of_scanners" --save-path "/mnt/projects/ri_scale/privagams"
@@ -260,7 +302,7 @@ def main() -> None:
     # disclaimer: based on testing, can be wrong
     MODEL_SIZE_GB = 4.8
     ONE_BATCH_SIZE_GB = 0.0113  # size of 1 tile 256x256
-    OVERHEAD = 2 # for pytorch and os stuff
+    OVERHEAD = 1 # for pytorch and os stuff
 
     VRAM_PER_WORKER = ((BATCH_SIZE * ONE_BATCH_SIZE_GB) + MODEL_SIZE_GB)
 
@@ -277,12 +319,18 @@ def main() -> None:
 
     start_time = time.time()
     if os.path.isdir(slide_path):
-        for slide_name in os.listdir(slide_path):
-            absolute_path = os.path.join(slide_path, slide_name)
-            if os.path.isdir(absolute_path) or absolute_path.endswith(".xml"):
-                continue
-            SLIDE_COUNT += 1
-            process_slide(absolute_path, save_path, DEVICE, MODEL_DTYPE, NUM_WORKERS, BATCH_SIZE, OVERRIDE)
+        for root, dirs, files in os.walk(slide_path):
+            if not root.split("/")[-1].startswith("."):
+                for file in files:
+                    if file.endswith(".svs"):
+                        root_folder = root.split("/")[-1]
+                        absolute_path = f"{root}/{file}"
+                        save_path_current = f"{save_path}/{root_folder}"
+                        print("Working on: "+ absolute_path, flush=True)
+                        print_memory_stats()
+
+                        SLIDE_COUNT += 1
+                        process_slide(absolute_path, save_path_current, DEVICE, MODEL_DTYPE, NUM_WORKERS, BATCH_SIZE, OVERRIDE)
     else:
         process_slide(slide_path, save_path, DEVICE, MODEL_DTYPE, NUM_WORKERS, BATCH_SIZE, OVERRIDE)
         SLIDE_COUNT += 1
@@ -298,4 +346,21 @@ def main() -> None:
     print(f"=============================================")
 
 if __name__ == "__main__":
+# Path to the library that fixed your 'jpeg12' error
+    PRELOAD_LIB = "/home/jb88526/.conda/envs/similarity-env/lib/libjpeg.so.8"
+    
+    runtime_env = {
+        "working_dir": ".",
+        "excludes": ["*"],
+        "env_vars": {
+            "LD_PRELOAD": PRELOAD_LIB,
+            "LD_LIBRARY_PATH": f"/home/jb88526/.conda/envs/similarity-env/lib:{os.environ.get('LD_LIBRARY_PATH', '')}"
+        }
+    }
+
+    logging.getLogger("ray").setLevel(logging.ERROR)
+    logging.getLogger("ray.data").setLevel(logging.ERROR)
+    logging.getLogger("ray._private.state_accelerator_v2").setLevel(logging.ERROR)
+
+    ray.init(runtime_env=runtime_env, logging_level=logging.ERROR, configure_logging=True)
     main()
